8eca192885c80249f4f4b72054860ee000822d5d
Borel Hall <borel@Borels-MacBook-Pro.local>
2017-01-26 18:01:02 -0500

    If block removed

--- Assignment 1.rkt
+++ Assignment 1.rkt
@@ -1,12 +1,20 @@
-;
+;Problem 1
 (DEFINE (reverse-general L)
-
         (COND; conditional block start
-         ((NULL? L) ()) ; is the list empty
-         ((NOT(LIST? CAR L))) ; is the first term of the list a literal?
-         (IF(LIST? CAR L) (CONS CDR L (reverse-general CAR L))
-            (CONS (reversee-general CDR L) CAR L)
-            )
+         ((NULL? L) '()) ; is the list empty
+         ;((NOT(LIST? CAR L))) ; is the first term of the list a literal?
+         (if (LIST? CAR L); if the first term of the list is another list...
+             (CONS (CDR L) (reverse-general (CAR L))) ; add the first 
+             (CONS (reverse-general (CDR L)) (CAR L))
+             )
          )
-            
-        )
+     )
+
+         
+
+(define (equation x)
+  (if (> x 2) 
+      (+ (- (* x x) x) 4)
+      (if (and (or (> x 1) (= x 1)) (or (< x 2) (= x 2)))
+          (/ 1 x)
+          0)))
--- Untitled.rkt
+++
@@ -1,5 +0,0 @@
-(DEFINE (reverse-general L)
-
-        
-        
-        )
